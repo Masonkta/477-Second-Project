e3051d19579ea610b9d3a3dee380974e310c32b3
Christian Salassi <102042316+Jouwsiz@users.noreply.github.com>
2025-04-10 14:41:24 -0500

    misc

---
+++ Escape Room/Assets/Scripts/ChessScripts/ChessSceneTransition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class ChessSceneTransition : MonoBehaviour
+{
+    private string sceneToLoad;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManaager.LoadScene(sceneToLoad);
+        }
+    }
+}
